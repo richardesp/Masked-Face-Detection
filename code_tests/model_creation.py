d578924c9c0212e5421fdfc5ddc9ef7fadf535ff
Ricardo Espantaleón <espantaleon2001@gmail.com>
2022-01-07 21:17:37 +0100

    model_creation unit test created

---
+++ code_tests/model_creation.py
@@ -0,0 +1,11 @@
+import unittest
+from models import model_01
+
+
+class MyTestCase(unittest.TestCase):
+    def test_creation_model(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
