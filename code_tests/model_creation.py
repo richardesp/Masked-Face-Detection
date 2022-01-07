c10bded6be8e6cca1b45255af35f288324cac485
Ricardo Espantaleón <espantaleon2001@gmail.com>
2022-01-08 00:05:31 +0100

    Updated model input in maskedfacepeople_config_vgg16.json

--- code_tests/model_creation.py
+++ code_tests/model_creation.py
@@ -1,10 +1,23 @@
 import unittest
 from models import model_01
+from utils import config
+import tensorflow as tf
+from utils import get_model_size
 
 
 class MyTestCase(unittest.TestCase):
-    def test_creation_model(self):
-        pass
+    @staticmethod
+    def test_creation_model():
+        config_file = config.process_config("../configs/maskedfacepeople_config_vgg16.json")
+        class_model = model_01.Model(config_file)
+        vgg_model = class_model.build_model()
+
+        # For check if any GPU is available
+        print(tf.config.list_physical_devices("GPU"))
+        # Batch_size = 10
+        print(f"{get_model_size.keras_model_memory_usage_in_bytes(vgg_model, 10) / 1000000000} GB")
+
+        vgg_model.summary()
 
 
 if __name__ == '__main__':
