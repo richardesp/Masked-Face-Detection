38e23b08eb366917f3d4988c53683b14ac24f5f7
Ricardo Espantaleón <espantaleon2001@gmail.com>
2022-01-09 21:10:54 +0100

    Compilation option was added

--- code_tests/training_test.py
+++ code_tests/training_test.py
@@ -12,8 +12,15 @@ from keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.optimizers import Adam
 
 config_file = config.process_config("../configs/maskedfacepeople_config_vgg16.json")
+
+# Allowing to get more GPU memory
+config = tf.compat.v1.ConfigProto()
+config.gpu_options.allow_growth = True
+session = tf.compat.v1.Session(config=config)
+
 class_model = model_01.Model(config_file)
-model = class_model.build_model()
+class_model.build_model(compilation=True)
+model = class_model.get_model()
 
 # Printing model summary for check if config file was loaded correctly
 model.summary()
@@ -24,6 +31,9 @@ callbacks = trainer.get_callbacks()
 directory = '/home/ricardo/Projects/maskedFaceDetection/dataset/dataset_1_0'
 
 batch_size = 50
+
+# Getting required model size
+print(f"{get_model_size.keras_model_memory_usage_in_bytes(model, batch_size) / 1000000000} GB required")
 
 train_datagen = ImageDataGenerator(validation_split=0.2,  # Splits the data into training (80%) and validation (20%)
                                    rescale=1. / 255,
@@ -54,11 +64,8 @@ validation_generator = train_datagen.flow_from_directory(
 
 class_names = ["with mask", "without mask"]
 
-config = tf.compat.v1.ConfigProto()
-config.gpu_options.allow_growth = True
-session = tf.compat.v1.Session(config=config)
-
-model.compile(optimizer=Adam(learning_rate=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
+# Model previously compiled
+# model.compile(optimizer=Adam(learning_rate=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
 
 # Pillow is required for work with PIL.Images
 # SciPy is required for image transformations
--- models/model_01.py
+++ models/model_01.py
@@ -7,15 +7,27 @@ VGG16 Convolutional Network for masked face people classification
 
 from base.base_model import BaseModel
 import tensorflow as tf
+from tensorflow.keras.optimizers import Adam
+from utils import get_learning_rate
 
 
 class Model(BaseModel):
     def __init__(self, config):
+        """
+        Parametrized constructor for initialize config file variable
+
+        :param config: Configuration file where read all models params required for the creation
+        """
         super(Model, self).__init__(config)
         self.build_model()
 
-    def build_model(self):
-        model = tf.keras.applications.VGG16(
+    def build_model(self, compilation=False):
+        """
+        Implemented function of the abstract class base_model, to create the specific instance of the model
+
+        :param compilation: Boolean to compile the resulting model
+        """
+        self.model = tf.keras.applications.VGG16(
             include_top=self.config.model.include_top,
             weights=self.config.model.weights,
             input_tensor=self.config.model.input_tensor,
@@ -26,4 +38,26 @@ class Model(BaseModel):
             classifier_activation=self.config.model.classifier_activation,
         )
 
-        return model
+        # If user indicated that wants the model compiled
+        if compilation:
+
+            # If learning rate param was specified by the user
+            if self.config.model.learning_rate is not None:
+
+                # We need to create a specified model.optimizer for call the __init__ function
+                if self.config.model.optimizer == "Adam":
+                    optimizer = Adam(learning_rate=self.config.model.learning_rate)
+
+                self.model.compile(optimizer=optimizer,
+                                   loss=self.config.model.loss,
+                                   metrics=['accuracy', get_learning_rate.get_lr_metric(optimizer)])
+
+            # If learning rate wasn't specified, we can use default constructor
+            else:
+
+                # We need to create a specified model.optimizer for call the __init__ function
+                if self.config.model.optimizer == "Adam":
+                    optimizer = Adam()
+
+                self.model.compile(optimizer=optimizer, loss=self.config.model.loss,
+                                   metrics=['accuracy', get_learning_rate.get_lr_metric(optimizer)])
